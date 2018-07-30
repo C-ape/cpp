c1f9fe7b03a50812e000c4196aba5523c7f4ee9f
C-ape <1091273274@qq.com>
2018-07-29 20:12:27 -0700

    add hello.cc

---
+++ hello.cc
@@ -0,0 +1,14 @@
+ ///
+ /// @file    hello.cc
+ /// @author  comeon(longhao41041.com)
+ /// @date    2018-07-29 20:11:09
+ ///
+ 
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main(void)
+{
+
+}
